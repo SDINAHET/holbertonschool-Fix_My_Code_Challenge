1dd7b82b6ab89419bb1abc4b75198a6e19d7b4e0
SDINAHET <stephane.dinahet@gmail.com>
2025-01-13 15:43:45 +0100

    Refactor add_dnodeint_end function for improved readability and structure

--- challenge/4-delete_dnodeint/add_dnodeint_end.c
+++ challenge/4-delete_dnodeint/add_dnodeint_end.c
@@ -17,23 +17,22 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
-	{
 		return (NULL);
-	}
 	new->n = n;
 	new->next = NULL;
+
 	if (*head == NULL)
 	{
-		*head = new;
 		new->prev = NULL;
+		*head = new;
 		return (new);
 	}
+
 	l = *head;
 	while (l->next != NULL)
-	{
 		l = l->next;
-	}
 	l->next = new;
 	new->prev = l;
+
 	return (new);
 }
